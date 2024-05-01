9c88dd5d3f8919e691ad9287ed3a470a189b7094
90 <hi@90.gripe>
2024-05-01 22:47:50 +0100

    Draw power when importing EMC into network

--- src/main/java/gripe/_90/appliede/strategy/EMCImportStrategy.java
+++ src/main/java/gripe/_90/appliede/strategy/EMCImportStrategy.java
@@ -7,6 +7,7 @@ import net.minecraft.server.level.ServerLevel;
 import appeng.api.behaviors.StackImportStrategy;
 import appeng.api.behaviors.StackTransferContext;
 import appeng.api.config.Actionable;
+import appeng.api.storage.StorageHelper;
 import appeng.util.BlockApiCache;
 
 import gripe._90.appliede.key.EMCKey;
@@ -40,9 +41,13 @@ public class EMCImportStrategy implements StackImportStrategy {
         var remainingTransfer = context.getOperationsRemaining() * EMCKeyType.TYPE.getAmountPerOperation();
         var inserted = Math.min(remainingTransfer, emcStorage.getStoredEmc());
 
-        context.getInternalStorage()
-                .getInventory()
-                .insert(EMCKey.BASE, inserted, Actionable.MODULATE, context.getActionSource());
+        StorageHelper.poweredInsert(
+                context.getEnergySource(),
+                context.getInternalStorage().getInventory(),
+                EMCKey.BASE,
+                inserted,
+                context.getActionSource(),
+                Actionable.MODULATE);
         emcStorage.extractEmc(inserted, IEmcStorage.EmcAction.EXECUTE);
 
         var opsUsed = Math.max(1, inserted / EMCKeyType.TYPE.getAmountPerOperation());
