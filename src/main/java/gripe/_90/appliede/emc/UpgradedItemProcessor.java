f42c82a4c37b878b196e59a7ad47cc92cb9962b6
90 <hi@90.gripe>
2024-05-01 16:15:51 +0100

    Split apart EMC processing for storage cells and general upgradeable items

--- src/main/java/gripe/_90/appliede/emc/UpgradedItemProcessor.java
+++ src/main/java/gripe/_90/appliede/emc/UpgradedItemProcessor.java
@@ -4,8 +4,7 @@ import java.math.BigInteger;
 
 import org.jetbrains.annotations.NotNull;
 
-import appeng.api.stacks.AEItemKey;
-import appeng.api.storage.StorageCells;
+import appeng.api.upgrades.IUpgradeableItem;
 
 import gripe._90.appliede.AppliedE;
 
@@ -16,32 +15,28 @@ import moze_intel.projecte.api.proxy.IEMCProxy;
 
 @SuppressWarnings("unused")
 @NBTProcessor
-public class StorageCellProcessor implements INBTProcessor {
+public class UpgradedItemProcessor implements INBTProcessor {
     @Override
     public String getName() {
-        return "AE2CellProcessor";
+        return "AE2UpgradeProcessor";
     }
 
     @Override
     public String getDescription() {
-        return "(AppliedE) Calculates EMC value of Applied Energistics 2 item cells depending on contents.";
+        return "(AppliedE) Calculates EMC value of Applied Energistics 2 items containing upgrades.";
     }
 
     @Override
     public long recalculateEMC(@NotNull ItemInfo itemInfo, long currentEmc) throws ArithmeticException {
-        var cell = StorageCells.getCellInventory(itemInfo.createStack(), null);
-
-        if (cell == null) {
+        if (!(itemInfo.getItem() instanceof IUpgradeableItem upgradeable)) {
             return currentEmc;
         }
 
+        var stack = itemInfo.createStack();
         var bigEmc = BigInteger.valueOf(currentEmc);
 
-        for (var key : cell.getAvailableStacks()) {
-            if (key.getKey() instanceof AEItemKey item) {
-                var keyEmc = IEMCProxy.INSTANCE.getValue(item.getItem());
-                bigEmc = bigEmc.add(BigInteger.valueOf(keyEmc).multiply(BigInteger.valueOf(key.getLongValue())));
-            }
+        for (var upgrade : upgradeable.getUpgrades(stack)) {
+            bigEmc = bigEmc.add(BigInteger.valueOf(IEMCProxy.INSTANCE.getValue(upgrade)));
         }
 
         return AppliedE.clampedLong(bigEmc);
