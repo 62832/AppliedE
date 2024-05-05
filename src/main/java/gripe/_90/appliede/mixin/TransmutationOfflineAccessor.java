daaf93fe8e1975d34e0558b7d483cb39637efe3d
90 <hi@90.gripe>
2024-05-05 22:15:38 +0100

    Provide an impromptu fix for server crashes involving dead players with modules See #3

---
+++ src/main/java/gripe/_90/appliede/mixin/TransmutationOfflineAccessor.java
@@ -0,0 +1,17 @@
+package gripe._90.appliede.mixin;
+
+import java.util.UUID;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+import moze_intel.projecte.api.capabilities.IKnowledgeProvider;
+import moze_intel.projecte.impl.TransmutationOffline;
+
+@Mixin(value = TransmutationOffline.class, remap = false)
+public interface TransmutationOfflineAccessor {
+    @Invoker
+    static IKnowledgeProvider invokeForPlayer(UUID uuid) {
+        throw new AssertionError();
+    }
+}
