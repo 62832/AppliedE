f1017ab317a3f330f9d644e0bd5c6d09bf066e7b
90 <hi@90.gripe>
2024-05-21 16:15:35 +0100

    Remove unnecessary mixin constructor

--- src/main/java/gripe/_90/appliede/mixin/aecapfix/EMCInterfacePartMixin.java
+++ src/main/java/gripe/_90/appliede/mixin/aecapfix/EMCInterfacePartMixin.java
@@ -3,19 +3,12 @@ package gripe._90.appliede.mixin.aecapfix;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
-import appeng.api.parts.IPartItem;
-import appeng.parts.AEBasePart;
-
 import gripe._90.aecapfix.AECapFix;
 import gripe._90.appliede.me.misc.EMCInterfaceLogic;
 import gripe._90.appliede.part.EMCInterfacePart;
 
 @Mixin(EMCInterfacePart.class)
-public abstract class EMCInterfacePartMixin extends AEBasePart implements AECapFix.Invalidator {
-    public EMCInterfacePartMixin(IPartItem<?> partItem) {
-        super(partItem);
-    }
-
+public abstract class EMCInterfacePartMixin implements AECapFix.Invalidator {
     @Shadow(remap = false)
     public abstract EMCInterfaceLogic getInterfaceLogic();
 
